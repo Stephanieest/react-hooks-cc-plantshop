71b5c2c4917988902e1447b612849e79dedbd778
Stephanie <stephnjonjo@gmail.com>
2024-11-11 18:00:22 +0300

    updates plantsy

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard({ plant, setPlants }) {
+  const { id, name, image, price } = plant;
+  const [isSoldOut, setIsSoldOut] = useState(false);
+
+
+  const toggleSoldOut = () => {
+    setIsSoldOut(!isSoldOut);
+  };
+
+
   return (
-    <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+    <li className="card">
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>Price: ${price}</p>
+      {isSoldOut ? (
+        <button className="secondary" onClick={toggleSoldOut}>
+          Out of Stock
+        </button>
       ) : (
-        <button>Out of Stock</button>
+        <button className="primary" onClick={toggleSoldOut}>
+          In Stock
+        </button>
       )}
     </li>
   );
--- src/components/Search.js
+++ src/components/Search.js
@@ -15,3 +15,5 @@ function Search() {
 }
 
 export default Search;
+
+
